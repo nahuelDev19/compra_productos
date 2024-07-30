6810dffa43c9317b9bb80f381df35c483eef8e4b
nahuelDev19 <142761564+nahuelDev19@users.noreply.github.com>
2024-07-30 20:42:10 -0300

    logica de user service v1

--- src/main/java/com/productos/compra_productos/service/implementservices/UserImplementService.java
+++ src/main/java/com/productos/compra_productos/service/implementservices/UserImplementService.java
@@ -1,38 +1,59 @@
 package com.productos.compra_productos.service.implementservices;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.productos.compra_productos.Entitys.RoleEntity;
 import com.productos.compra_productos.Entitys.UserEntity;
 import com.productos.compra_productos.dtos.dto_request.UserRequest;
 import com.productos.compra_productos.dtos.dto_response.UserResponse;
+import com.productos.compra_productos.repository.UserRepository;
 import com.productos.compra_productos.service.services.UserService;
 
 @Service
-
 public class UserImplementService implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public UserResponse create(UserRequest rq) {
         UserEntity user= new UserEntity();
+        user.setName(rq.getName());
+        user.setLastname(rq.getLastname());
+        user.setEmail(rq.getEmail());
+        user.setPassword(rq.getPassword());
+        user.setAge(rq.getAge());
+        user.setRoles(rq.getRoles());
+        userRepository.save(user);
         return this.convertidorResponse(user);
     }
 
     @Override
     public UserResponse update(UserRequest rq, Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        UserEntity user= userRepository.findById(id).orElseThrow();
+        user.setName(rq.getName());
+        user.setLastname(rq.getLastname());
+        user.setEmail(rq.getEmail());
+        user.setPassword(rq.getPassword());
+        user.setAge(rq.getAge());
+        user.setRoles(rq.getRoles());
+        userRepository.save(user);
+        return this.convertidorResponse(user);
     }
 
     @Override
     public UserResponse read(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'read'");
+        UserEntity user= userRepository.findById(id).orElseThrow();
+        return this.convertidorResponse(user);
+
     }
 
     @Override
     public void delete(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        UserEntity user= userRepository.findById(id).orElseThrow();
+        userRepository.delete(user);
+
     }
 
 
