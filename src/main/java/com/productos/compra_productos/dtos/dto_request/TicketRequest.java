9be79fd25bc347f171c9c507a5a5101c735c3f39
nahuelDev19 <142761564+nahuelDev19@users.noreply.github.com>
2024-07-30 20:09:11 -0300

    creacion de ticket response y request

--- src/main/java/com/productos/compra_productos/dtos/dto_request/TicketRequest.java
+++ src/main/java/com/productos/compra_productos/dtos/dto_request/TicketRequest.java
@@ -1,5 +1,27 @@
 package com.productos.compra_productos.dtos.dto_request;
 
+import java.time.LocalDate;
+import java.util.Set;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TicketRequest {
+
+
+    private Long id;
+    private LocalDate date;
+    private UserRequest userRequest;
+    private Integer total;
+    private Set<ProductRequest> products;
+
 
 }
--- src/main/java/com/productos/compra_productos/dtos/dto_response/TicketResponse.java
+++ src/main/java/com/productos/compra_productos/dtos/dto_response/TicketResponse.java
@@ -1,5 +1,23 @@
 package com.productos.compra_productos.dtos.dto_response;
 
+import java.time.LocalDate;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TicketResponse {
+
+        private UserResponse user;
+        private LocalDate date;
+        private Integer total;
+        private Set<ProductResponse> products;
+
 
 }
