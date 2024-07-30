fc6bd6a305c065f39a9a839982ec2996076e5fa6
nahuelDev19 <142761564+nahuelDev19@users.noreply.github.com>
2024-07-30 19:52:05 -0300

    creacion de Produc response y request

--- src/main/java/com/productos/compra_productos/dtos/dto_request/ProductRequest.java
+++ src/main/java/com/productos/compra_productos/dtos/dto_request/ProductRequest.java
@@ -1,5 +1,20 @@
 package com.productos.compra_productos.dtos.dto_request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ProductRequest {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Integer price;
+
 
 }
--- src/main/java/com/productos/compra_productos/dtos/dto_response/ProductResponse.java
+++ src/main/java/com/productos/compra_productos/dtos/dto_response/ProductResponse.java
@@ -1,5 +1,18 @@
 package com.productos.compra_productos.dtos.dto_response;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ProductResponse {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Integer price;
 
 }
