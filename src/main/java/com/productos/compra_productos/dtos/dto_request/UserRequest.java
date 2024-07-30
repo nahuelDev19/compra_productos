3e7dfc5be23e26b8c6ba5c5c3173772ffe42947f
nahuelDev19 <142761564+nahuelDev19@users.noreply.github.com>
2024-07-30 20:20:06 -0300

    creacion de User response y request

--- src/main/java/com/productos/compra_productos/dtos/dto_request/UserRequest.java
+++ src/main/java/com/productos/compra_productos/dtos/dto_request/UserRequest.java
@@ -1,5 +1,25 @@
 package com.productos.compra_productos.dtos.dto_request;
 
+import java.util.Set;
+
+import com.productos.compra_productos.Entitys.RoleEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserRequest {
+
+    private Long id;
+    private String name;
+    private String lastname;
+    private Integer age;
+    private String email;
+    private String password;
+    private Set<RoleEntity> roles;
 
 }
--- src/main/java/com/productos/compra_productos/dtos/dto_response/UserResponse.java
+++ src/main/java/com/productos/compra_productos/dtos/dto_response/UserResponse.java
@@ -1,5 +1,26 @@
 package com.productos.compra_productos.dtos.dto_response;
 
+import java.util.Set;
+
+import com.productos.compra_productos.Entitys.RoleEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserResponse {
+
+    private Long id;
+    private String name;
+    private String lastname;
+    private Integer age;
+    private String email;
+    private String password;
+    private Set<RoleEntity> roles;
 
 }
